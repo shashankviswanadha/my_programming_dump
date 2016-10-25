179b8181521cfd717877cc4f0de4b3f5db3a8b22
Shashank <sashankviswanadha@gmail.com>
2016-10-25 22:38:05 +0530

    Added multi-level page tables

--- OS-lab/week5/paging.c
+++ OS-lab/week5/paging.c
@@ -63,12 +63,31 @@ struct tlb_entry{
   int vpn,pfn,valid,read,write,execute;
 };
 
+struct page_directory_entry{
+  int pte_address,valid;
+};
+
 int extract_vpn(int virtual_address){
   int VPN_MASK = 64512;
   int SHIFT = 10;
   //Extract the VPN from the virtual address  
   int VPN = (virtual_address & VPN_MASK) >> SHIFT;
   return VPN;
+}
+
+int extract_pdi(int vpn){
+  int PDI_MASK = 60;
+  int SHIFT = 2;
+  int PDI = (vpn & PDI_MASK) >> SHIFT;
+  return PDI;
+}
+
+
+int extract_pte_offset(int virtual_address){
+  int MASK = 3072;
+  int SHIFT = 10;
+  int PDI = (virtual_address & MASK) >> SHIFT;
+  return PDI;
 }
 
 int extract_offset(int virtual_address){
@@ -118,41 +137,86 @@ int access_mem(struct tlb_entry *tl,struct page_table_entry *pte,int virtual_add
     }
     else{
       if (tl[i].vpn == vpn){
-        printf("%d%s\n",tl[i].vpn,"yoo" );
+        //printf("%d%s\n",tl[i].vpn,"yoo" );
         return get_physical_address(tl[i].pfn,off);
       }
     }
   }
-
-  fn = get_frame_number(pte,virtual_address);
-  int j;
-  for (j = 0;j <15; j++){
-    tl[j] = tl[j+1];
-  }
-  tl[15].vpn = vpn;
-  tl[15].pfn = fn;
-  printf("%d%s\n",tl[15].vpn,"yoyo");
-  if (pte[vpn].valid == 0){
+  if (pte[vpn].valid != 0){
+    fn = get_frame_number(pte,virtual_address);
+    int j;
+    for (j = 0;j <15; j++){
+      tl[j] = tl[j+1];
+    }
+    tl[15].vpn = vpn;
+    tl[15].pfn = fn;
+    //printf("%d%s\n",tl[15].vpn,"yoyo");
+    
     //raise(SIGSEGV);
     //RaiseException(SEGMENTATION_FAULT);
-  }
-  else{
+    
     return get_physical_address(tl[15].pfn,off);
   }
-
 }
 
+int AccessMemory(struct page_directory_entry *pd,struct tlb_entry *tl,struct page_table_entry *pte,int virtual_address){
+  int vpn = extract_vpn(virtual_address);
+  int off = extract_offset(virtual_address);
+  int fn,pa;
+  int i;
+  for (i = 0; i<16; i++){
+    if (pte[vpn].valid == 0){
+    //raise(SIGSEGV);
+    //RaiseException(SEGMENTATION_FAULT);
+    }
+    else{
+      if (tl[i].vpn == vpn){
+        //printf("%d%s\n",tl[i].vpn,"yoo" );
+        return get_physical_address(tl[i].pfn,off);
+      }
+    }
+  }
+  int pdi = extract_pdi(vpn);
+  printf("%s%d\n","pdi:",pdi );
+  if (pd[pdi].valid == 0){
+    //raise(SIGSEGV);
+    //RaiseException(SEGMENTATION_FAULT);
 
-  /*  else if (CanAccess(PTE.ProtectBits) == False)
-      RaiseException(PROTECTION_FAULT)
-      }*/
+  }
+  else {
+    int pte_off = extract_pte_offset(virtual_address);
+    printf("pteoff:%d\n",pte_off);
+    int pti = pd[pdi].pte_address + pte_off;
+    printf("pti:%d\n",pti );
+    fn = get_frame_number(pte,virtual_address);
+    if (pte[pti].valid != 0){
+      int j;
+      for (j = 0;j <15; j++){
+        tl[j] = tl[j+1];
+      }
+      tl[15].vpn = pti;
+      tl[15].pfn = fn;
+      //printf("%d%s\n",tl[15].vpn,"yoyo");
+      
+      //raise(SIGSEGV);
+      //RaiseException(SEGMENTATION_FAULT);
+      return get_physical_address(tl[15].pfn,off);
+    }
 
+    
+  }
+  
+
+}
+  
 
 void main(){
   struct page_table_entry *page_table;
   struct tlb_entry *tlb;
+  struct page_directory_entry *page_dir;
   page_table = (struct page_table_entry*)malloc(sizeof(struct page_table_entry) * 64);
   tlb = (struct tlb_entry*)malloc(sizeof(struct tlb_entry) * 16);
+  page_dir = (struct page_directory_entry*)malloc(sizeof(struct page_directory_entry) * 16);
   int *random_num;
   int i;
   random_num = random_integers(64,64); //6
@@ -160,14 +224,37 @@ void main(){
     page_table[i].frame_number = random_num[i];
     //printf("%d\n",page_table[i].frame_number );
   }
+  int j = 0;
+  int c ;
+  for (c = 0; c < 16 ; c++){
+    page_dir[c].pte_address = j;
+    j = j + 4;
+  }
 
   page_table[20].frame_number = 50; 
   page_table[20].valid = 1;
   page_table[21].frame_number = 51;
   page_table[21].valid = 1;
+
+  int k,l;
+  for(k = 0; k < 16; k++){
+    int flag = 0;
+    for (l = 0; l < 4; l++){
+      if (page_table[(4*k)+l].valid == 1){
+        page_dir[k].valid = 1;
+        flag = 1;
+        break;
+      }
+    }
+    if (flag == 0){
+      page_dir[k].valid = 0;
+    }
+  }
+
+
   //20480 is the virtual address for vpn 20
-  int ph = access_mem(tlb,page_table,20480);
-  int tr = access_mem(tlb,page_table,21504);
+  int ph = AccessMemory(page_dir,tlb,page_table,20480);
+  int tr = AccessMemory(page_dir,tlb,page_table,21504);
   //long t = (long)extract_vpn(20481);
   //long f = (long)extract_offset(20481);
   //long fn = (long)get_frame_number(page_table,20481);
@@ -177,9 +264,9 @@ void main(){
   //printf("Offset:  %zu\n",to_binary(f));
   //printf("Frame number:  %zu\n",to_binary(fn));
   printf("Physical address:  %zu\n",to_binary(ph));
-  int t = access_mem(tlb,page_table,20480);
+  int t = AccessMemory(page_dir,tlb,page_table,20480);
   printf("Physical address:  %zu\n",to_binary(tr));
-  printf("%d%s\n",tlb[14].vpn,"oooo");
+  //printf("%d%s\n",tlb[14].vpn,"oooo");
 }
 
 
