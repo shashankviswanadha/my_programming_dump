5e97046c0831927ea079eaf0c1710ac2c28eee36
Shashank <sashankviswanadha@gmail.com>
2016-10-28 00:24:27 +0530

    prog proj 1 done

--- OS-lab/Prog_project/shell.c
+++ OS-lab/Prog_project/shell.c
@@ -7,168 +7,6 @@
 #include <sys/wait.h>
 #include <sys/resource.h>
 
-/*struct process{
-  int pid,background;
-  char** arguments;
-};
-
-
-struct node {
-  struct process *pro;
-  struct node *next;
-};
-
-struct node * find(struct node *root, int value ) {
-  if (root -> pro -> pid == value){
-    return root;
-
-  }
-  else {
-    if (root -> next != 0){
-      find ((root -> next), value);
-  
-    }
-    else{
-      struct node *notfound;
-      notfound = (struct node *) malloc( sizeof(struct node) );
-      struct process *nf;
-      notfound = (struct node *) malloc( sizeof(struct node) );
-      nf->pid = -1;
-      notfound->next = 0;
-      notfound->pro = nf;  
-      return notfound;
-
-    }
-
-  }
-
-}
-void printlinkedlist(struct node *root){
-  if (root->next != 0){
-    printf("%s\n",root->pro->arguments[0] );
-    root = root->next;
-    printlinkedlist(root);
-  }
-  if (root->pro != 0){
-    printf("%s\n",root->pro->arguments[0] );
-  }
-}
-
-
-
-void append(struct node *root, struct process *pro){
-  if (root->next == 0 && root->pro == 0){
-    root->pro = pro;
-  }
-  else{
-  struct node *new,*curr = root;
-  while(curr->next != 0){
-    curr = curr->next;
-
-  }
-  
-  new = (struct node *) malloc( sizeof(struct node) );
-  new->next = 0;
-  curr->next = new;
-  new->pro = pro;
-  }
-}
-
-                
-void insert(struct node *root, int pos, struct process *pro){
-  int count = 0;
-  int flag = 0;
-  struct node *temp, *new, *curr = root;
-  new = (struct node *) malloc( sizeof(struct node) );
-  if (pos == 0){
-    pos = 1;
-    flag = 1;
-  }
-  while (pos-1 != count){
-    curr = curr->next;
-    count ++;
-  }
-  new->pro = pro;
-  temp = curr->next;
-  curr->next = new;
-  new->next = temp;
-  if (flag == 1){
-    struct process *t = root->pro;
-    root->pro = (root->next)->pro;
-    (root->next)->pro = t;
-  
-  }
-  
-  }
-  
-//delete a node
-/*void delete(struct node *root, int pos){
-  int count = 0, flag = 0;
-  struct node *temp, *curr = root;
-  if (pos == 0){
-    pos = 1;
-    flag = 1;
-  }
-  if (flag == 1){
-    int t = root->x;
-    root->x = (root->next)->x;
-    (root->next)->x = t;
-  }
-
-  while (pos-1 != count){
-    curr = curr->next;
-    count ++;
-  }
-  temp = curr->next;
-  curr->next = (curr->next)->next;
-  free(temp);
-}
-
-
-
-
-// delete entire list
-void delete_all(struct node *root){
-  struct node *temp, *curr = root;
-  while (curr->next != 0){
-    temp = curr->next;
-    free(curr);
-    curr = temp;
-
-  }
-  
-  free(curr);
-
-
-  }
-
-
-
-
-
-
-
-
-
-
-void create_process(struct process* child,char* args[100],int len){
-  int back = 0;
-  if (strcmp(args[len-1],"&") == 0){
-      args[len-1] = NULL;
-      back = 1;
-    }
-  else{
-      args[len] = NULL;
-
-    }
-
-  child = (struct process *) malloc( sizeof(struct process));
-  child->background = back;
-  child->arguments = args;
-
-} */
-
-
 int pidd(char (*allnames)[100],int allpids[100],int no_all,char (*currnames)[100],int currpids[100],int no_curr,char* arguments[100]){
   int i = 0;
   if (arguments[1] == NULL){
@@ -200,7 +38,6 @@ int pidd(char (*allnames)[100],int allpids[100],int no_all,char (*currnames)[100
 
 }
 
-
 int hist(char (*allnames)[100],int allpids[100],int no_all,char* arguments[100]){
   int i;
   if (arguments[1] == NULL){
@@ -215,9 +52,6 @@ int hist(char (*allnames)[100],int allpids[100],int no_all,char* arguments[100])
       printf("%s\n",allnames[i]);
     }
   }
-
-
-
 
 }
 
@@ -243,13 +77,25 @@ int CURR_COUNT ;
   return;
   }*/
 
-void handler(char (*allnames)[100],int allpids[100],int no_all)
-{ //printf("yolo\n");
+void my_sigchld_handler(){
+  pid_t pid;
+  int   status;
+  while ((pid = waitpid(-1, &status, WNOHANG)) != -1){
+    printf("yoloo\n");//printf("%s\n",curr_names[0]);// Or whatever you need to do with the PID
+  }
+  return;
+}
+
+
+
+/*void handler(){ //printf("yolo\n");
   int wstat;
   //union wait wstat;
   pid_t pid;
   int status;
-  int i,hit = no_all;
+  int i,hit = CURR_COUNT;
+  printf("%s\n",curr_names[0]);
+  printf("%d\n",curr_pid[0] );
   while (1) {
     pid = wait3 (&wstat, WNOHANG, (struct rusage *)NULL  );
     if (pid == 0)
@@ -257,29 +103,29 @@ void handler(char (*allnames)[100],int allpids[100],int no_all)
     else if (pid == -1)
       return;
     else{
-      printf("%d  %d\n",pid,no_all);
-      for (i = 0; i < no_all; i++){
+      //printf("%d  %d\n",pid,CURR_COUNT );
+      for (i = 0; i < CURR_COUNT ; i++){
         if (curr_pid[i] = (int)pid){
           printf("yyyyyy\n");
           hit = i;
           break;
         }
       }
-      for (i = hit; i < no_all - 1; i++){
-        allpids[i] = allpids[i+1];
-        allnames[i] = allnames[i+1];
+      for (i = hit; i < CURR_COUNT - 1; i++){
+        curr_pid[i]= curr_pid[i+1];
+        *curr_names[i] = *curr_names[i+1];
     
       }
-      no_all --;
+      CURR_COUNT --;
       return ;
     }
   }
 
-}
+  }*/
 
 
 void main(){
-  //signal(SIGCHLD, handler);
+  //signal(SIGCHLD, my_sigchld_handler);
   char* username = getenv("USER");
   char hostname[30];
   gethostname(hostname, 30);
@@ -295,14 +141,33 @@ void main(){
   curr_names = malloc(sizeof *curr_names * 1024);
   int curr_pid[100];
   int CURR_COUNT = 0;
-  struct sigaction sa;
-  memset(&sa, 0, sizeof(sa));
-  sa.sa_handler = handler(curr_names,curr_pid,CURR_COUNT);
-  sigaction(SIGCHLD, &sa, NULL);
+  char cwd[1024];
+  char pd[1024];
+  char *strLocation;
+  getcwd(pd,sizeof(pd));
+  printf("%s\n",pd );
+  pid_t p;
+  int   status;
 
   while(1){
-    
-    printf("%s@%s ~ $ ",username,hostname );
+    pid_t p;
+    int   status;
+    if (getcwd(cwd,sizeof(cwd)) != NULL){
+      if (strcmp(pd,cwd) == 0){
+        fprintf(stdout,"%s@%s ~ > ",username,hostname);
+      }
+      else {
+        strLocation = strstr(cwd,pd);  
+        if (strLocation != NULL){
+          strcpy (strLocation, strLocation + strlen (pd));
+          fprintf(stdout,"%s@%s ~ %s> ",username,hostname,strLocation);
+        }
+        else {
+          fprintf(stdout,"%s@%s ~ %s> ",username,hostname,cwd);
+          
+        }
+      }
+    }
     fgets(inp,1024,stdin);
     size_t length = strlen(inp);
     if (inp[length - 1] == '\n'){
@@ -332,29 +197,36 @@ void main(){
     else{
       args[len] = NULL;
     }
+
     if(strcmp(args[0],"!hist") == 0) {
       args[0] = hist_names[(*args[1] - '0') - 1];
       args[1] = NULL;
     }
-    if (strcmp(args[0],"pid") == 0){
+    else if (strcmp(args[0],"pid") == 0){
       pidd(hist_names,hist_pid,PROCESS_COUNT,curr_names,curr_pid,CURR_COUNT,args);
     }
+    
     else if(strcmp(args[0],"hist") == 0 ){
       hist(hist_names,hist_pid,PROCESS_COUNT,args);
     }
     else{
       pid = fork();
       if(pid == 0){
-        
-        execvp(args[0],args);
-        fprintf(stderr, "Child process could not do execvp\n");
-        //exit(0);
+        if (strcmp(args[0],"cd") != 0){
+          execvp(args[0],args);
+          fprintf(stderr, "Child process could not do execvp\n");
+          //exit(0);
+        }
+        exit(0);
       }
       else{
+        if (strcmp(args[0],"cd") == 0){
+          chdir(args[1]);
+          //exit(0);
+        }
         if(background == 0){
           int status = 0;
           //printf("%d\n",pid );
-          
           if (waitpid(pid,&status,0) == -1){
             perror( "waitpid" );
           }
@@ -368,7 +240,7 @@ void main(){
           CURR_COUNT++;
           background = 0;
         }
-        printf("%d  %d\n",PROCESS_COUNT,CURR_COUNT );
+        //printf("%d  %d\n",PROCESS_COUNT,CURR_COUNT );
         //printf("Child exited\n");
         
         //printlinkedlist(root);
